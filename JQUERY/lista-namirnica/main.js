d94e948e2fa0a2c3fde0f7b364e850590a8174af
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-04-02 17:12:08 +0200

    lista namirnica app

--- JQUERY/lista-namirnica/main.js
+++ JQUERY/lista-namirnica/main.js
@@ -1,1 +1,24 @@
-$('h1').hide(2000);
+var ispisNamirnica = "";
+
+$('.dodavanje').on('click', function() {
+    // na klik treba da preuzmemo vrednost iz input polja
+    var noviUnos = $('.namirnica').val();
+    if (noviUnos != '') {
+        ispisNamirnica = "<tr>";
+        ispisNamirnica += "<td>"+ noviUnos + "</td>";
+        ispisNamirnica += "<td><button class='btn btn-warning brisanje'>Brisanje</button></td>";
+        ispisNamirnica += "</tr>";
+        $('.lista-namirnica tbody').append(ispisNamirnica); // dodajemo u DOM novi unos
+        $('.namirnica').val('').focus();
+    } else {
+        alert('Niste uneli artikal za kupovinu');
+    }
+    // $('.brisanje').on('click', function() {
+    //     // $(this).parent().parent('tr').remove();
+    //     $(this).closest('tr').remove();
+    // });
+});
+
+$('tbody').on('click', '.brisanje', function() {
+    $(this).closest('tr').remove();
+});

f239ef382df0a950a4062c5c3e047affd2faf467
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-04-03 20:44:25 +0200

    tehnologija

--- webcon/js/main.js
+++ webcon/js/main.js
@@ -27,4 +27,24 @@ var waypoint = new Waypoint({
         progress[3].setAttribute("style","width: 90%; transition-duration: 4s;")
     },
     offset: '90%'
-  })
+})
+// owl slider
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:100,
+    //nav:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 1500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})

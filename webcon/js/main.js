5254bd5d9a4bcbcb546e11a48731cd6f9647285a
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-04-03 13:48:00 +0200

    sticky navigation

--- webcon/js/main.js
+++ webcon/js/main.js
@@ -1,3 +1,4 @@
+// typed biblioteka
 var typed = new Typed('.element', {
     strings: ['Kodiranje uzivo sredom', '30 dana kodiranja', 'JS radionica', 'Custom WP radionica'],
     loop: true,
@@ -5,4 +6,13 @@ var typed = new Typed('.element', {
     smartBackspace: true,
     backSpeed: 100,
     loopCount: Infinity
-  });
+});
+// navigacija
+$(window).on('scroll', function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 100) {
+        $('nav').addClass('stickyAdd');
+    } else {
+        $('nav').removeClass('stickyAdd');
+    }
+});

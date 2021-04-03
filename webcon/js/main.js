f63e5485a593d04e092d579dc056d438d77f7cfa
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-04-03 12:25:30 +0200

    header

---
+++ webcon/js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.element', {
+    strings: ['Kodiranje uzivo sredom', '30 dana kodiranja', 'JS radionica', 'Custom WP radionica'],
+    loop: true,
+    typeSpeed: 100,
+    smartBackspace: true,
+    backSpeed: 100,
+    loopCount: Infinity
+  });

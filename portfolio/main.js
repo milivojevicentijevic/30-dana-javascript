99a6832921b007cc7a30be114f9998aa432d349e
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-04-07 20:31:50 +0200

    kontakt

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,3 +1,4 @@
+// nav scroll
 $(window).on('scroll', function() {
     var scroll = $(window).scrollTop();
     if(scroll >= 80) {
@@ -6,3 +7,16 @@ $(window).on('scroll', function() {
         $('.navbar').removeClass('sticky');
     }
 });
+
+// tipkajuci naslov u headeru
+const naslov = "Full Stack Web Developer";
+let i = 0;
+
+const simulacijaKucanja = () => {
+    if (i < naslov.length) {
+        document.querySelector('.tipkajuciNaslov').innerHTML += naslov.charAt(i);
+        i++;
+        setTimeout(simulacijaKucanja, 150);
+    }
+}
+simulacijaKucanja();

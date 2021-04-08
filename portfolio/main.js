d55bd1d41399ce8f564359b9bb4d1206639750fb
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-04-08 21:27:27 +0200

    responsive

--- portfolio/main.js
+++ portfolio/main.js
@@ -20,3 +20,13 @@ const simulacijaKucanja = () => {
     }
 }
 simulacijaKucanja();
+
+// hamburger meni
+function toggleDropdown() {
+    let navbarToggle = document.getElementById('navbar-toggle');
+    if (navbarToggle.className === 'topnav') {
+        navbarToggle.className += ' responsive';
+    } else {
+        navbarToggle.className = 'topnav';
+    }
+}
